4c37f8ebe7d6360ee92032d0dc8bbd296e85f670
Werbth Rocha <werbth@gmail.com>
2022-05-12 21:39:47 -0300

    Update machine

--- provision.aur.sh
+++ provision.aur.sh
@@ -7,7 +7,6 @@ cd "${userhome}"
 
 # copy configuration files
 rm -rf .bashrc .vimrc .xinitrc .Xresources .i3status.conf .i3
-cp /vagrant/.bashrc .
 cp /vagrant/.vimrc .
 cp /vagrant/.xinitrc .
 cp /vagrant/.Xresources .
@@ -26,6 +25,13 @@ git config --global core.eol lf
 #git config --global user.name <your name>
 #git config --global user.email <your email>
 
+# install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg --noconfirm -si
+cd ../
+rm -rf yay/
+
 # install fonts
 git clone https://aur.archlinux.org/consolas-font.git
 cd consolas-font/
@@ -34,26 +40,3 @@ sudo pacman --noconfirm -U consolas-font-*
 cd ../
 rm -rf consolas-font/
 
-# install skype
-git clone https://aur.archlinux.org/skypeforlinux-beta-bin.git
-cd skypeforlinux-beta-bin/
-makepkg --noconfirm -si
-sudo pacman --noconfirm -U skypeforlinux-beta-bin-*
-cd ../
-rm -rf skypeforlinux-beta-bin/
-
-# install slack
-git clone https://aur.archlinux.org/slack-desktop.git
-cd slack-desktop/
-makepkg --noconfirm -si
-sudo pacman --noconfirm -U slack-desktop-*
-cd ../
-rm -rf slack-desktop/
-
-# install visual studio code
-git clone https://aur.archlinux.org/visual-studio-code.git
-cd visual-studio-code/
-makepkg --noconfirm -si
-sudo pacman --noconfirm -U visual-studio-code-*
-cd ../
-rm -rf visual-studio-code/
